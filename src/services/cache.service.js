8b4c76940ef5ed8b649ef32e2c206ff32b7babad
nahuetomas <nrodriguez@portinos.com.ar>
2024-10-17 11:28:20 -0300

    [REFACTOR] DEVLAR-1980 - Refactor cache

--- src/services/cache.service.js
+++ src/services/cache.service.js
@@ -1,39 +1,111 @@
-// cacheHelper.js
-const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = require("../../config");
+const { createClient } = require("redis");
+const {
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_PASSWORD,
+  REDIS_USERNAME,
+} = require("../../config");
 
-const redis = require("redis");
+/**
+ * Represents a Redis service for interacting with Redis.
+ *
+ * @class
+ * @requires redis
+ */
+class Redis {
+  /**
+   * Create a RedisService instance.
+   *
+   * @constructor
+   * @param {Object} options - The configuration options for the Redis client
+   * @param {string} options.host - The Redis server hostname
+   * @param {number} options.port - The Redis server port number
+   * @param {string} [options.password] - The Redis server password (optional)
+   * @param {string} [options.username] - The Redis server username (optional)
+   */
+  constructor({ host, port, password, username }) {
+    this.redisClient = createClient({
+      username,
+      password,
+      socket: {
+        host,
+        port,
+      },
+    });
 
-const redisClient = redis.createClient({
+    // Handle errors in the Redis client
+    this.redisClient.on("error", (err) => {
+      console.error("Redis client error:", err);
+    });
+
+    // Connect to Redis once when the instance is created
+    this.redisClient.connect();
+  }
+
+  /**
+   * Set the value of a key in the Redis database.
+   *
+   * @async
+   * @param {string} key - The Redis key to set
+   * @param {*} value - The value to set the key to
+   * @returns {Promise<void>} - A promise that resolves when the operation completes
+   * @throws {Error} If the Redis operation fails
+   */
+  async set(key, value) {
+    await this.redisClient.set(key, value);
+  }
+
+  /**
+   * Deletes the specified key from Redis.
+   *
+   * @async
+   * @param {string} key - The key to delete from Redis
+   * @returns {Promise<void>} - A promise that resolves when the key is deleted
+   * @throws {Error} If the Redis operation fails
+   */
+  async del(key) {
+    await this.redisClient.del(key);
+  }
+
+  /**
+   * Retrieves the value associated with the specified key from Redis.
+   *
+   * @async
+   * @param {string} key - The key to retrieve the value for
+   * @returns {Promise<any>} - A promise that resolves to the value associated with the key, or null if the key does not exist
+   * @throws {Error} If the Redis operation fails
+   */
+  async get(key) {
+    const data = await this.redisClient.get(key);
+    return data;
+  }
+
+  /**
+   * Retrieves all the keys stored in Redis.
+   *
+   * @async
+   * @returns {Promise<string[]>} - A promise that resolves to an array of keys
+   * @throws {Error} If the Redis operation fails
+   */
+  async getAll() {
+    const keys = await this.redisClient.keys("*");
+    return keys;
+  }
+
+  /**
+   * Close the connection to Redis.
+   *
+   * @async
+   * @returns {Promise<void>} - A promise that resolves when the connection is closed
+   */
+  async close() {
+    await this.redisClient.quit();
+  }
+}
+
+module.exports = new Redis({
   host: REDIS_HOST,
-  port: REDIS_PORT,
   password: REDIS_PASSWORD,
+  port: REDIS_PORT,
+  username: REDIS_USERNAME,
 });
-
-exports.set = (key, value) => {
-  return new Promise((resolve, reject) => {
-    redisClient.set(key, value, (err) => {
-      if (err) reject(err);
-      else resolve(true);
-    });
-  });
-};
-
-exports.del = (key) => {
-  return new Promise((resolve, reject) => {
-    redisClient.del(key, (err) => {
-      if (err) reject(err);
-      else resolve(true);
-    });
-  });
-};
-
-exports.get = (key) => {
-  return new Promise((resolve, reject) => {
-    redisClient.get(key, (err, val) => {
-      if (err || val == null) {
-        if (err) reject(err);
-        else resolve(false); // key is missing / not exists
-      } else resolve(val);
-    });
-  });
-};
