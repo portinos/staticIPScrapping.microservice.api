b7ccb6c47d63172d922f0fde8870dd668f30de3f
nahuetomas <nrodriguez@portinos.com.ar>
2024-10-17 11:31:53 -0300

    [COMMENT] DEVLAR-1980 - Disable cron

--- src/cron-jobs/index.js
+++ src/cron-jobs/index.js
@@ -1,12 +1,12 @@
-const {
-  walmartProductsCron,
-} = require("../modules/walmart-products/walmart-product.cron");
+// const {
+//   walmartProductsCron,
+// } = require("../modules/walmart-products/walmart-product.cron");
 
 // Execute crons
 const executeCrons = () => {
-  walmartProductsCron.start();
+  // walmartProductsCron.start();
 };
 
-walmartProductsCron.start();
+// walmartProductsCron.start();
 
 module.exports = { executeCrons };
