ec2ab90f872ff09ad3721695c1fc84860215c0d6
nahuetomas <nrodriguez@portinos.com.ar>
2024-10-17 10:22:48 -0300

    [ADD] DEVLAR-1980 - Config global variables

---
+++ config/index.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+module.exports = {
+  // Base config
+  PORT: process.env.PORT || "3000",
+  REDIS_PORT: process.env.REDIS_PORT || "6379",
+  REDIS_HOST: process.env.REDIS_HOST || "127.0.0.1",
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD || "",
+
+  // Modules
+  WALMART_DATA:
+    process.env.WALMART_DATA ||
+    "https://strapi.staging.portinos.com/v1/api/walmart/get_raw",
+};
